0d6f0aad1f2cd1854a8cf0a6358773fbad01648b
nullchinchilla <nullchinchilla@pm.me>
2021-05-14 19:37:54 -0500

    revert

--- src/taskqueue.rs
+++ src/taskqueue.rs
@@ -1,115 +1,102 @@
-use std::sync::Arc;
+use std::cell::UnsafeCell;
 
 use async_task::Runnable;
 
-use concurrent_queue::ConcurrentQueue;
-use parking_lot::Mutex;
-use rtrb::{Consumer, Producer, PushError, RingBuffer};
+use crossbeam_deque::{Injector, Steal, Stealer, Worker};
 
 #[derive(Debug)]
 pub struct GlobalQueue {
-    inner: ConcurrentQueue<Runnable>,
+    inner: Injector<Runnable>,
 }
 
 impl Default for GlobalQueue {
     fn default() -> Self {
         Self {
-            inner: ConcurrentQueue::unbounded(),
+            inner: Injector::default(),
         }
     }
 }
 
 impl GlobalQueue {
     pub fn push(&self, task: Runnable) {
-        self.inner.push(task).unwrap();
+        self.inner.push(task)
     }
 
     pub fn pop(&self) -> Option<Runnable> {
-        self.inner.pop().ok()
+        loop {
+            match self.inner.steal() {
+                Steal::Retry => continue,
+                Steal::Empty => return None,
+                Steal::Success(v) => return Some(v),
+            }
+        }
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct LocalQueueHandle {
-    inner: Arc<Mutex<Consumer<Runnable>>>,
+    inner: Stealer<Runnable>,
 }
 
 #[derive(Debug)]
 pub struct LocalQueue {
-    pusher: Producer<Runnable>,
-    popper: Arc<Mutex<Consumer<Runnable>>>,
-    // next_task: Option<Runnable>,
+    inner: Worker<Runnable>,
+    next_task: Option<Runnable>,
 }
-
-unsafe impl Send for LocalQueue {}
 
 impl Default for LocalQueue {
     fn default() -> Self {
-        let (pusher, popper) = RingBuffer::new(256).split();
         Self {
-            pusher,
-            popper: Arc::new(Mutex::new(popper)),
-            // next_task: Default::default(),
+            inner: Worker::new_fifo(),
+            next_task: Default::default(),
         }
     }
 }
 
 impl LocalQueue {
+    #[inline]
     pub fn push(&mut self, is_yield: bool, task: Runnable) -> Result<(), Runnable> {
         // if this is the same task as last time, we don't push to next_task
         // if is_yield {
-        self.pusher.push(task).map_err(|e| match e {
-            PushError::Full(e) => e,
-        })?;
-        // } else if let Some(task) = self.next_task.replace(task) {
-        //     self.pusher.push(task).map_err(|e| match e {
-        //         PushError::Full(e) => e,
-        //     })?;
+        self.inner.push(task);
+        // } else {
+        //     let next_task = self.next_task();
+        //     if let Some(task) = next_task.replace(task) {
+        //         self.inner.push(task);
+        //     }
         // }
         Ok(())
     }
 
+    #[inline]
     pub fn pop(&mut self) -> Option<Runnable> {
-        // if let Some(next_task) = self.next_task.take() {
+        // let next_task = self.next_task();
+        // if let Some(next_task) = next_task.take() {
         //     Some(next_task)
         // } else {
-        self.popper.lock().pop().ok()
+        self.inner.pop()
         // }
     }
 
-    pub fn steal_global(&mut self, other: &GlobalQueue) {
-        if !self.pusher.is_full() {
-            let steal_cnt = other.inner.len() / 2 + 1;
-            let remaining_cap = self.pusher.slots();
-            for _ in 0..steal_cnt.min(remaining_cap) {
-                if let Some(popped) = other.pop() {
-                    self.pusher.push(popped).unwrap();
-                } else {
-                    return;
-                }
-            }
-        }
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.inner.len()
     }
 
-    pub fn steal_local(&mut self, other: &LocalQueueHandle) {
-        if !self.pusher.is_full() {
-            if let Some(mut other) = other.inner.try_lock() {
-                let steal_cnt = other.slots() / 2 + 1;
-                let remaining_cap = self.pusher.slots();
-                for _ in 0..steal_cnt.min(remaining_cap) {
-                    if let Ok(popped) = other.pop() {
-                        self.pusher.push(popped).unwrap();
-                    } else {
-                        return;
-                    }
-                }
-            }
-        }
+    #[inline]
+    pub fn steal_global(&self, other: &GlobalQueue) {
+        std::iter::repeat_with(|| other.inner.steal_batch(&self.inner)).find(|v| !v.is_retry());
     }
 
+    #[inline]
+    pub fn steal_local(&self, other: &LocalQueueHandle) {
+        let _ = other.inner.steal_batch(&self.inner);
+    }
+
+    #[inline]
     pub fn handle(&self) -> LocalQueueHandle {
         LocalQueueHandle {
-            inner: self.popper.clone(),
+            inner: self.inner.stealer(),
         }
     }
 }
