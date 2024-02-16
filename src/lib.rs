40a13d209ba75a658359d92dfb6ba801631aafc4
james7132 <contact@jamessliu.com>
2024-02-16 13:48:05 -0800

    Weaken the atomic orderings for notification

--- src/lib.rs
+++ src/lib.rs
@@ -541,7 +541,7 @@ impl State {
     fn notify(&self) {
         if self
             .notified
-            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+            .compare_exchange(false, true, Ordering::Release, Ordering::Acquire)
             .is_ok()
         {
             let waker = self.sleepers.lock().unwrap().notify();
@@ -673,7 +673,7 @@ impl Ticker<'_> {
 
         self.state
             .notified
-            .swap(sleepers.is_notified(), Ordering::SeqCst);
+            .store(sleepers.is_notified(), Ordering::Release);
 
         true
     }
@@ -687,7 +687,7 @@ impl Ticker<'_> {
 
             self.state
                 .notified
-                .swap(sleepers.is_notified(), Ordering::SeqCst);
+                .store(sleepers.is_notified(), Ordering::Release);
         }
     }
 
@@ -735,7 +735,7 @@ impl Drop for Ticker<'_> {
 
             self.state
                 .notified
-                .swap(sleepers.is_notified(), Ordering::SeqCst);
+                .store(sleepers.is_notified(), Ordering::Release);
 
             // If this ticker was notified, then notify another ticker.
             if notified {
