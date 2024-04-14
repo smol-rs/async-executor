c4b59c50c51fef8a197f97399cddceea61b860c6
James Liu <contact@jamessliu.com>
2024-04-13 23:21:46 -0700

    Make the doc_auto_cfg a crate-level attribute.
    Co-authored-by: John Nunley <jtnunley01@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,8 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/smol-rs/smol/master/assets/images/logo_fullsize_transparent.png"
 )]
-#[cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
 use std::fmt;
 use std::marker::PhantomData;
 use std::panic::{RefUnwindSafe, UnwindSafe};
